a640aea65212163bd957063212114241c72f7461
mllrdev-joanne-ferrolino <joanne.ferrolino@mllrdev.com>
2020-10-05 12:32:13 +0800

    updated vue 2 project to vue 3

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <table class="table-auto border px-4 py-2">
+    <slot></slot>
+    <table class="justify-center text-base border border-gray-500 ">
       <thead>
         <tr>
           <th
-            class="border border-gray-400 px-4 py-2 text-gray-800"
-            v-for="(header, index) in columnHeaders"
+            class="px-4 py-4 bg-white"
+            v-for="(header, index) in formattedHeaders"
             :key="index"
           >
-            {{ header | capitalize }}
+            {{ header }}
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(row, index) in data" :key="index">
-          <td v-for="(header, index) in columnHeaders" :key="index">
+        <tr
+          v-for="(row, index) in data"
+          :key="index"
+          class="py-4 bg-white border border-gray-500 hover:bg-blue-300"
+        >
+          <td v-for="(header, index) in columnHeaders" :key="index" class="">
             {{ row[header] }}
           </td>
         </tr>
@@ -24,36 +29,32 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "@vue/composition-api";
+import { defineComponent, computed, reactive } from "vue";
 export default defineComponent({
   name: "data-table",
   props: {
     values: {
-      type: Array as any,
+      type: Object,
     },
   },
-  filters:{
-    capitalize: function(value: string) {
-      if(!value) return '';
-      value = value.toString();
-      return value.charAt(0).toUpperCase() + value.slice(1);
-    }
-  },
-  setup(prop) {
-    const data = prop.values ?? [];
+  setup(props) {
+    const data = reactive(props.values ?? []);
     const columnHeaders = computed(() => Object.keys(data[0]));
 
+    function formatHeaders(word: string) {
+      const newWord = word.charAt(0).toUpperCase() + word.slice(1);
+      return newWord.replace(/([A-Z])/g, " $1").trim();
+    }
+    const formattedHeaders = computed(() =>
+      columnHeaders.value.map((x) => formatHeaders(x))
+    );
     return {
       data,
-      columnHeaders
+      columnHeaders,
+      formattedHeaders,
     };
   },
 });
 </script>
 
-<style lang="scss" scoped>
-table th:hover {
-  background: #717699;
-  color: #ffffff;
-}
-</style>
+<style lang="scss" scoped></style>
